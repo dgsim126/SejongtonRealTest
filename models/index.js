f85854d9e7ce1f4a5ee2ad29ea4265bf86145089
dgsim <dgsim126@gmail.com>
2024-07-15 15:08:22 +0900

    init setting

--- models/index.js
+++ models/index.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
 const db = {}
-const sequelize = new Sequelize('sejongton', 'user', '1234', {
-  host: 'db',
-  port: 3308,
+const sequelize = new Sequelize('sejongton', 'user', '1234', { // 일부로 하드코딩한 것이니 건드리지 말 것
+  host: 'localhost',
   dialect: 'mariadb', // MariaDB 사용
   logging: false // 쿼리 로깅 비활성화
 });
@@ -11,5 +11,7 @@ const sequelize = new Sequelize('sejongton', 'user', '1234', {
 // db 객체에 각 모델 및 Sequelize 설정 추가
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
+
+
 
 module.exports = db;
--- server.js
+++ server.js
@@ -1,45 +1,34 @@
-require('dotenv').config();
-const express = require('express');
-// const cookieParser = require('cookie-parser');
-const { sequelize } = require("./models/index");
-// const surveyRouter = require("./routers/surveyRoute");
-// const postRouter= require("./routers/postRoute");  
-// const cors = require('cors');
+    require('dotenv').config();
+    const express = require('express');
+    const path = require('path');
+    const { sequelize } = require("./models/index");
 
-const app = express();
-const port = 3308;
+    const app = express();
+    const port = 8080;
 
-sequelize
-.sync({ force: false })
-.then(()=>{
-    console.log('데이터베이스 연결 성공')
-}).catch(err=>{
-    console.log(err)
-})
+    // 데이터베이스 연결
+    sequelize
+    .sync({ force: false })
+    .then(()=>{
+        console.log('데이터베이스 연결 성공');
+    }).catch(err=>{
+        console.log(err);
+    });
 
-// EJS 뷰 엔진 설정
-// app.set('view engine', 'ejs');
+    // EJS를 뷰 엔진으로 설정
+    app.set('view engine', 'ejs');
+    app.set('views', path.join(__dirname, 'views'));
 
-// 미들웨어 설정
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // URL-encoded 데이터 파싱
+    // 미들웨어 설정
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true })); // URL-encoded 데이터 파싱
 
-// const corsOptions = {
-//   origin: 'http://localhost:3000', // React 앱의 주소
-//   credentials: true, // 쿠키를 포함한 요청을 허용
-// };
+    // 기본 라우트 설정
+    app.get('/', (req, res) => {
+        res.render('index');
+    });
 
-// app.use(cors(corsOptions));
-// app.use(express.json())
-// app.use(cookieParser());
-// app.use('/api', require('./routers/profileRoute'));
-// app.use("/api", require("./routers/loginRoute"))
-// app.use('/api/survey', require("./routers/surveyRoute"))
-// app.use('/api/comments', require('./routers/commentRoute'));
-// app.use('/api/votes', require('./routers/voteRoute'));
-// app.use('/api/posts', postRouter);
-
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+    // 서버 시작
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
