09d218b435deb97d41cff8bf3194d73957fbc608
dgsim <dgsim126@gmail.com>
2024-07-18 20:00:06 +0900

    folder setting

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const db = {}
 const sequelize = new Sequelize('sejongton', 'user', '1234', { // 일부로 하드코딩한 것이니 건드리지 말 것
   host: 'localhost',
   dialect: 'mariadb', // MariaDB 사용
-  logging: false // 쿼리 로깅 비활성화
+  logging: false, // 쿼리 로깅 비활성화
 });
 
 // db 객체에 각 모델 및 Sequelize 설정 추가
