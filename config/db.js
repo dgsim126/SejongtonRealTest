d0414b2f7113d56cadb6a04e87741a485b92235d
dgsim <dgsim126@gmail.com>
2024-07-19 14:19:16 +0900

    db.js hardcoding fix

--- config/db.js
+++ config/db.js
@@ -1,12 +1,17 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const db = {}
-const sequelize = new Sequelize('sejongton', 'user', '1234', { // 일부로 하드코딩한 것이니 건드리지 말 것
-  host: 'localhost',
-  dialect: 'mariadb', // MariaDB 사용
-  logging: true // 쿼리 로깅 비활성화
-});
+const db = {};
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE, 
+  process.env.DB_USER, 
+  process.env.DB_PASSWORD, 
+  {
+    host: process.env.DB_HOST, 
+    dialect: 'mariadb', // MariaDB 사용
+    logging: true // 쿼리 로깅 비활성화
+  }
+);
 
 // db 객체에 각 모델 및 Sequelize 설정 추가
 db.Sequelize = Sequelize;
