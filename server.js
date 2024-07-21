b41ad1e309f506eec4d32a327d366174b64d9b3f
Shihyun Han <devowl99@gmail.com>
2024-07-22 05:42:16 +0900

    commit for merge

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 const port = 8080;
 
 // 데이터베이스 연결
-sequelize.sync({ force: false })
+sequelize.sync({ force: true })
 .then(()=>{
     console.log('데이터베이스 연결 성공');
 }).catch(err=>{
