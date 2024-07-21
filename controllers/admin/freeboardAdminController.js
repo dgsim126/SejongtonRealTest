66bcfa42d90debf8a7cc5cb484e0ef5452355537
dgsim <dgsim126@gmail.com>
2024-07-21 22:11:25 +0900

    admin delete complete

--- controllers/admin/freeboardAdminController.js
+++ controllers/admin/freeboardAdminController.js
@@ -27,20 +27,27 @@ const showAll = asyncHandler(async (req, res) => {
 const deletePost = asyncHandler(async (req, res) => {
     const { key } = req.params;
 
-    try{
-        const deleteData= await FreeBoard.destroy({
+    try {
+        // 댓글 삭제
+        await FreeBoardComment.destroy({
+            where: { freeboardkey: key }
+        });
+
+        // 게시글 삭제
+        const deleteData = await FreeBoard.destroy({
             where: { key }
         });
-        if(deleteData === 0){
+
+        if (deleteData === 0) {
             return res.status(404).json({ message: "Post not found." });
         }
-        res.status(200).json({ message: "Post deleted successfully." });
-    }catch(error){
+
+        res.status(200).json({ message: "Post and associated comments deleted successfully." });
+    } catch (error) {
         // 서버 에러
         console.error('Error deleting post:', error);
-        res.status(500).json({ message: "An error occurred while deleting the post." });
+        res.status(500).json({ message: "An error occurred while deleting the post and comments." });
     }
-    
 });
 
 module.exports = { showAll, deletePost };
